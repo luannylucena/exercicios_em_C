4d3a3571814dd1711325406b5c32973e9ffe1fa9
luannylucena <medeirosluanny@gmail.com>
2023-03-07 00:26:12 -0300

    update

---
+++ rotone/rotone.c
@@ -0,0 +1,10 @@
+// Escreva um programa que receba uma string e a exiba, substituindo cada uma de suas letras pela próxima em ordem alfabética.
+
+//'z' torna-se 'a' e 'Z' torna-se 'A'. O caso permanece inalterado.
+
+//A saída será seguida por um \n.
+
+//Se o número de argumentos não for 1, o programa exibirá \n.
+
+#include <unistd.h>
+
