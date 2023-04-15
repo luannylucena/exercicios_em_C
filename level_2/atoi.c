9b01384d128395027b6158b9dc12b840d8785082
Luanny Medeiros Lucena <lmedeiro@c1r2p1.42sp.org.br>
2023-04-15 19:52:30 -0300

    add atoi

---
+++ level_2/atoi.c
@@ -0,0 +1,22 @@
+
+int	ft_atoi(const char *str)
+{
+    int i = 0;
+    int nb = 0;
+    int signal;
+    
+    while(str[i] == ' ' || str[i] == '\t')
+        i++;
+    while(str[i] == '-' || str[i] == '+') //se for sinal
+    {
+        if(str[i] == '-')
+            signal = signal * (-1);
+        i++;
+    }
+    while(str[i] >= '0' && str[i] <= '9')
+    {
+        nb = nb * 10  + (str[i] - '0'); 
+        i++;
+    }
+    return(nb * signal);
+}
