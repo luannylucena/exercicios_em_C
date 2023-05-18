a32d74ed883814029903eb4807510ae7cb236bfc
Luanny Medeiros Lucena <lmedeiro@c3r11p2.42sp.org.br>
2023-05-18 20:57:43 -0300

    update ft_strrev

--- level_2/ft_strrev.c
+++ level_2/ft_strrev.c
@@ -7,31 +7,46 @@ Sua função deve ser declarada da seguinte forma: char *ft_strrev(char *str);
 
 */
 
+#include <stdio.h>
+#include <unistd.h>
+
 int ft_strlen(char *str)
 {
     int i = 0;
-
-    while(str[i] != '\0')
-    {
-        i++;
-    }
+    
+    while(str[i])
+       i++;
     return(i);
 }
 
 char *ft_strrev(char *str)
 {
-    int i = 0;
     int len;
-    char aux;
+    int i = 0;
+    int aux;
+
+    len = ft_strlen(str); // pq começa do zero. 
 
     while(i <= len/2)
     {
-        len = ft_strlen(str);
         aux = str[i];
         str[i] = str[len - 1];
         str[len - 1] = aux;
-        i++;
-        len--;
+        i++; // vou acrescentando 1
+        len--; // e diminuindo outro.. pois troca
     }
     return(str);
 }
+
+//int main(int argc, char **argv)
+//{
+//   char *novastr;
+
+//    if(argc != 2)
+//    {
+//        write(1,"\n", 1);
+//        return (1);
+//    }
+//    novastr = ft_strrev(argv[1]);
+//    printf("%s\n", novastr);
+//}
