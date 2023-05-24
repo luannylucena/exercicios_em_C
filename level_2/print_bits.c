55119faf42195b2abd49602eb791230d4105a5ed
Luanny Medeiros Lucena <lmedeiro@c1r1p3.42sp.org.br>
2023-05-24 16:30:02 -0300

    print_bits

--- level_2/print_bits.c
+++ level_2/print_bits.c
@@ -5,38 +5,16 @@ Sua função deve ser declarada da seguinte forma:
 
 void print_bits(unsigned char octet);
 
-Exemplo, se passar 2 para print_bits, imprimirá "00000010"
-
 */
-
-/* void ft_putnbr(int nb)
-{
-    char    *numeros = "0123456789";
-
-    if (nb > 9)
-        ft_putnbr(nb / 10);
-    write(1, &numeros[nb % 10], 1);
-}
- */
-
-#include <unistd.h>
 
 void print_bits(unsigned char octet)
 {
-    int qtd = 7; //pq já começo no 1, né..
-
-    while(qtd >= 0) //esse while serve somente para parar a impressão do numero, posise snao ele vai continuar netrando no if e imprimindo infinito.
+    int i = 8;
+    unsigned char bit;
+    
+    while(i--)
     {
-        if((octet >> qtd & 1) == 0) //faço o &(e) com 1 pq se for 0 com aqualquer coisa é 0. o que importa no & é o 1.
-            write(1, "0", 1);
-        else
-            write(1, "1", 1);
-        qtd--;
+        bit = (octet >> i & 1) + '0';
+        write(1, &bit, 1);
     }
 }
-
-/* int main()
-{
-    print_bits(3);
-    return(0);
- */
