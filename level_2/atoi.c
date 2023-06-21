c66c488d8efa935460aa411032ec411b1a807e6b
Luanny Medeiros Lucena <lmedeiro@c1r4p3.42sp.org.br>
2023-06-21 14:28:04 -0300

    update

--- level_2/atoi.c
+++ level_2/atoi.c
@@ -1,13 +1,14 @@
+#include <stdio.h>
 
 int	ft_atoi(const char *str)
 {
     int i = 0;
     int nb = 0;
     int signal = 1;
-    
-    while(str[i] == ' ' || str[i] == '\t')
+
+    while(str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
         i++;
-    while(str[i] == '-' || str[i] == '+') //se for sinal
+    if(str[i] == '+' || str[i] == '-')
     {
         if(str[i] == '-')
             signal = signal * (-1);
@@ -15,8 +16,17 @@ int	ft_atoi(const char *str)
     }
     while(str[i] >= '0' && str[i] <= '9')
     {
-        nb = nb * 10  + (str[i] - '0'); 
+        nb = nb * 10 + (str[i] - '0');
         i++;
     }
     return(nb * signal);
+}
+
+int main()
+{
+    int str;
+
+    str = ft_atoi("-11113423");
+    printf("%d", str);
+    return(0);
 }
--- level_2/max.c
+++ level_2/max.c
@@ -3,7 +3,7 @@ int max(int *tab, unsigned int len)
     unsigned int i = 0;
     unsigned int result;
 
-    result = tab[i];
+    result = tab[i]; // atribui o valor do primeiro elemento do array "tab" à variável "result"
     while(i < len)
     {
         if(result < tab[i])
