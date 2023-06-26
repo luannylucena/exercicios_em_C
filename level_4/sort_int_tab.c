ad38811eeeba887670897402341653d004603213
luannylucena <medeirosluanny@gmail.com>
2023-06-26 19:57:04 -0300

    update

--- level_4/sort_int_tab.c
+++ level_4/sort_int_tab.c
@@ -14,21 +14,5 @@ Input is always coherent.
 
 void sort_int_tab(int *tab, unsigned int size)
 {
-    unsigned int i = 0;
-    int temp;
-
-    while(i < (size - 1))
-    {
-        if(tab[i] > tab[i + 1])
-        {
-            temp = tab[i];
-            tab[i] = tab[i + 1];
-            tab[i + 1] = temp;
-            i = 0;
-        }
-        else
-            i++;
-    }
-
     
 }
