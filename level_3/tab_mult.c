045a3226487b3ef54cbde96cd866ec37eee5e48d
luannylucena <medeirosluanny@gmail.com>
2023-06-30 16:53:08 -0300

    tab_mult

--- level_3/tab_mult.c
+++ level_3/tab_mult.c
@@ -5,8 +5,8 @@ void ft_putnbr(int num)
     char str[10] = "0123456789";
 
     if(num > 9)
-        ft_putnbr(num/10);
-    write(1, str[num%10], 1);
+        ft_putnbr(num / 10);
+    write(1, &str[num % 10], 1);
 }
 
 int ft_atoi(char *str)
@@ -25,10 +25,42 @@ int ft_atoi(char *str)
             i++;
         }
     }
+
     if(str[i] >= '0' && str[i] <= '9')
     {
         nb = nb * 10 + (str[i] - '0');
         i++;
     }
     return(signal * nb);
+}
+
+int main(int argc, char **argv)
+{
+    int i = 0;
+    int nb;
+
+    if(argc != 2)
+    {
+        write(1, "\n", 1);
+        return(0);
+    }
+
+    nb = ft_atoi(argv[1]);
+    
+    if(nb <= 0) //se for negativo ou igual a zero.
+    {
+        write(1, "\n", 1);
+        return(0);
+    }
+    while(i < 10)
+    {
+        ft_putnbr(i); // colocamos o número do índice multiplicador
+        write(1, " x ", 3); // espaço multiplicação e espaço
+        ft_putnbr(nb); // colocamos o número 
+        write(1, " = ", 3); // espaço igual e espaço
+        ft_putnbr(i * nb); // colocamos o número do resultado da multiplicação
+        write(1, "\n", 1); // colocamos a quebra de linha
+        i++;  // repetimos 9 vezes
+    }
+    return(0);
 }
